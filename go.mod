1889a6ce336117c0b66ab374162a3d2649cf9700
Sebastian Radloff <sradloff23@gmail.com>
2020-04-07 14:54:32 -0400

    Loads creds from config

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/sebradloff/monday-gcal-integration
 go 1.14
 
 require (
-	github.com/douglasmakey/oauth2-example v0.0.0-20190217053201-b19e764d1950 // indirect
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
 	google.golang.org/api v0.21.0
 )
--- main.go
+++ main.go
@@ -2,22 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/sebradloff/monday-gcal-integration/handlers"
 )
 
 func main() {
-	server := &http.Server{
-		Addr:    fmt.Sprintf(":8080"),
-		Handler: handlers.New(),
-	}
-
-	log.Printf("Starting HTTP Server. Listening at %q", server.Addr)
-	if err := server.ListenAndServe(); err != http.ErrServerClosed {
-		log.Printf("%v", err)
-	} else {
-		log.Println("Server closed!")
-	}
+	c := LoadConfig(true)
+	fmt.Println(c)
 }
